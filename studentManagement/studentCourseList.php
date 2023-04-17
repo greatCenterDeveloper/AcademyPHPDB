857c36d90369d27e27c4fe5bbcedeb2cf2950ecc
swj2023 <qkedkfkvkd@gmail.com>
2023-04-17 13:06:42 +0900

    학생 상세정보 출결 횟수 추가

--- studentManagement/studentCourseList.php
+++ studentManagement/studentCourseList.php
@@ -22,13 +22,59 @@
             AND t.id = '$teacherId'";
     
     $result = mysqli_query($db, $sql);
+
     if($result) {
         $rowNum = mysqli_num_rows($result);
         $rows = array();
 
         for($i=0; $i<$rowNum; $i++) {
             $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            $rows[$i] = $row;
+
+            $rowTemp = array();
+            $rowTemp['profile'] = $row['profile'];
+            $rowTemp['course'] = $row['course'];
+            $rowTemp['teacher'] = $row['teacher'];
+            $rowTemp['studentId'] = $row['studentId'];
+            $rowTemp['studentName'] = $row['studentName'];
+
+            $courseCode = $row['course'];
+
+            $attendanceSql  =  "SELECT DISTINCT
+                                        COUNT(ca.attendance_state) AS attendance
+                                FROM student AS s,
+                                     course_attendance AS ca,
+                                     course_request AS cr
+                                WHERE ca.course_request_code = cr.course_request_code
+                                AND cr.course_code = s.course_code
+                                AND cr.student_id = s.id
+                                AND s.course_code = '$courseCode'
+                                AND s.id = '$studentId'
+                                AND ca.attendance_state='출석'";
+            
+            $attendanceResult = mysqli_query($db, $attendanceSql);
+            $attendanceRow = mysqli_fetch_array($attendanceResult, MYSQLI_ASSOC);
+
+            $rowTemp['attendance'] = $attendanceRow['attendance'];
+            
+
+            $absenceSql  = "SELECT DISTINCT
+                                    COUNT(ca.attendance_state) AS absence
+                            FROM student AS s,
+                                course_attendance AS ca,
+                                course_request AS cr
+                            WHERE ca.course_request_code = cr.course_request_code
+                            AND cr.course_code = s.course_code
+                            AND cr.student_id = s.id
+                            AND s.course_code = '$courseCode'
+                            AND s.id = '$studentId'
+                            AND ca.attendance_state='결석'";
+            
+            $absenceResult = mysqli_query($db, $absenceSql);
+            $absenceRow = mysqli_fetch_array($absenceResult, MYSQLI_ASSOC);
+            
+            $rowTemp['absence'] = $absenceRow['absence'];
+
+            $rows[$i] = $rowTemp;
         }
         
         echo json_encode($rows);
